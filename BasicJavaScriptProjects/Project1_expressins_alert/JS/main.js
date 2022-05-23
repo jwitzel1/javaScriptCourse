f891a45324c2e25e8d519f02842fe8c284bf3c7a
jwitzel1 <103449965+jwitzel1@users.noreply.github.com>
2022-05-23 08:16:38 -0700

    Update main.js

--- BasicJavaScriptProjects/Project1_expressins_alert/JS/main.js
+++ BasicJavaScriptProjects/Project1_expressins_alert/JS/main.js
@@ -1,10 +1,10 @@
-window.alert("Hello, World! win alert")
-document.write("Hello, World! doc write")
-document.write("One and One and One "+"is three...");
-var C = "text"+" ... glue ... " + "text";
-document.write(C);
-var x1=10, x2=30, x3=50, y1="hello", y2="howdy", y3="hi";
-var y1=y1.fontcolor("red");
-var y2=y2.fontcolor("green");
-var y3.y3.fontcolor("yellow");
-var z=3+3;
+window.alert("Hello, World! win alert"); //windows alert use
+document.write("Hello, World! doc write"); //doc write use
+document.write("One and One and One "+"is three...");  //concat
+var C = "text"+" ... glue ... " + "text"; //concat with a variable
+document.write(C); // writing the above
+var x1=10, x2=30, x3=50, y1="hello", y2="howdy", y3="hi"; // comma separated variable declaration
+var y1=y1.fontcolor("red"); // assigning colors to variable entity 1
+var y2=y2.fontcolor("green"); // assigning colors to variable entity 2
+var y3.y3.fontcolor("yellow"); // assigning colors to variable entity 3
+var z=3+3; // expression (not statement)
