d7a70357df77c338c45f706f7bcf5b479cd0c62f
jwitzel1 <103449965+jwitzel1@users.noreply.github.com>
2022-05-23 05:09:08 -0700

    Update main.js

--- BasicJavaScriptProjects/Project1_expressins_alert/JS/main.js
+++ BasicJavaScriptProjects/Project1_expressins_alert/JS/main.js
@@ -1,3 +1,5 @@
 window.alert("Hello, World! win alert")
 document.write("Hello, World! doc write")
 document.write("One and One and One "+"is three...");
+var C = "text"+" ... glue ... " + "text";
+document.write(C);
