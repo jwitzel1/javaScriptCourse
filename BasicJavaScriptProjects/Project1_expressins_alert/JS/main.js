3c2eded169988fb30090656d20f46ac982c89364
jwitzel1 <103449965+jwitzel1@users.noreply.github.com>
2022-05-23 05:13:36 -0700

    Update main.js

--- BasicJavaScriptProjects/Project1_expressins_alert/JS/main.js
+++ BasicJavaScriptProjects/Project1_expressins_alert/JS/main.js
@@ -4,3 +4,6 @@ document.write("One and One and One "+"is three...");
 var C = "text"+" ... glue ... " + "text";
 document.write(C);
 var x1=10, x2=30, x3=50, y1="hello", y2="howdy", y3="hi";
+var y1=y1.fontcolor("red");
+var y2=y2.fontcolor("green");
+var y3.y3.fontcolor("yellow");
