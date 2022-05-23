3705ef974e53d30f2f73a86ea18e3f30653ff653
jwitzel1 <103449965+jwitzel1@users.noreply.github.com>
2022-05-23 08:19:37 -0700

    Update main.js

--- BasicJavaScriptProjects/Project1_expressins_alert/JS/main.js
+++ BasicJavaScriptProjects/Project1_expressins_alert/JS/main.js
@@ -3,6 +3,10 @@ document.write("Hello, World! doc write"); //doc write use
 document.write("One and One and One "+"is three...");  //concat
 var C = "text"+" ... glue ... " + "text"; //concat with a variable
 document.write(C); // writing the above
+var D = "t1";
+var E = "t2";
+var F = D + " & " + E;
+document.write(D + " & " + E + "is the same as "+ F);
 var x1=10, x2=30, x3=50, y1="hello", y2="howdy", y3="hi"; // comma separated variable declaration
 var y1=y1.fontcolor("red"); // assigning colors to variable entity 1
 var y2=y2.fontcolor("green"); // assigning colors to variable entity 2
