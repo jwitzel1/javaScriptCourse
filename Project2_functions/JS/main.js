5b6388a85e695e7b7ffcf49d60df20e35c1f2690
jwitzel1 <jason.s.witzel@gmail.com>
2022-05-30 07:51:44 -0700

    project2

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function findMe(){
+    var A="I am id1 and this is variable A";
+    var B="I am id2 and this is variable B";
+    var C="Now you know my names!";
+    
+    document.getElementById("id1").innerHTML=A;
+    document.getElementById("id2").innerHTML=B;
+    document.getElementById("id3").innerHTML=C;
+   
+}
