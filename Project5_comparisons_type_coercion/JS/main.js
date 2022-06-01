f2b755073d6e454d2bc2ada83c05355e2e6d7b1c
jwitzel1 <jason.s.witzel@gmail.com>
2022-06-01 16:02:02 -0700

    project5

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,6 @@
+function whatAmI(){
+    var whatIam=typeof "What Am I?";
+ //   document.getElementById("id1").innerHTML = "This is " + whatIam + "<br />";
+    document.write("This was a " + whatIam);
+
+}
